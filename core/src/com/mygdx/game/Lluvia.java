b548f4ef0b5a9631ea0611d4fb9a87a5efd5cd0d
nelsoneitor12 <nelson.fuentes.l@mail.pucv.cl>
2022-10-28 13:51:44 -0300

    Codigo unificado

--- core/src/com/mygdx/game/Lluvia.java
+++ core/src/com/mygdx/game/Lluvia.java
@@ -10,57 +10,66 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
-public class Lluvia {
+public class Lluvia implements TipoObstaculo{
 	private rainDropsPos gotasPos;
 	private rainDropsType gotasTipo;
     private long lastDropTime;
     private Texture gotaBuena;
     private Texture gotaMala;
     private Texture gotaMuyBuena;
+    private Texture granizo;
     private Sound dropSound;
     private Music rainMusic;
+    
 	   
-	public Lluvia(Texture gotaBuena, Texture gotaMala,Texture gotaMuyBuena, Sound ss, Music mm ) {
-		rainMusic = mm;
-		dropSound = ss;
-		this.gotaBuena = gotaBuena;
-		this.gotaMala = gotaMala;
-		this.gotaMuyBuena = gotaMuyBuena;
-		gotasPos = new rainDropsPos();
-		gotasTipo = new rainDropsType();
+	public Lluvia() {
+		
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+        
+		this.gotaBuena = new Texture(Gdx.files.internal("drop.png"));
+		this.gotaMala = new Texture(Gdx.files.internal("dropBad.png"));
+		this.gotaMuyBuena= new Texture(Gdx.files.internal("VeryGoodDrop.png"));
+		this.granizo= new Texture(Gdx.files.internal("hielo.png"));
 	}
 	
 	public void crear() {
-		crearGotaDeLluvia();
-	    // start the playback of the background music immediately
-	    rainMusic.setLooping(true);
-	    rainMusic.play();
+		gotasPos = new rainDropsPos();
+		gotasTipo = new rainDropsType();
+		crearObjetoObstaculo();
+	      // start the playback of the background music immediately
+	      rainMusic.setLooping(true);
+	      rainMusic.play();
 	}
 	
-	private void crearGotaDeLluvia() {
+	public void crearObjetoObstaculo() {
 	      Rectangle raindrop = new Rectangle();
-	      raindrop.x = MathUtils.random(0, 800-64);
-	      raindrop.y = 480;
-	      raindrop.width = 64;
-	      raindrop.height = 64;
+	      raindrop.x = MathUtils.random(0, 1280-32);
+	      raindrop.y = 720;
+	      raindrop.width = 32;
+	      raindrop.height = 32;
 	      gotasPos.addGota(raindrop);
 	      // ver el tipo de gota
-	      
+
+
 	      int numRand = MathUtils.random(1,100);
 	      if (numRand <= 60)	    	  
 	    	  gotasTipo.addGotaType(2);
-	      if (numRand > 60 &&  numRand < 98)
+	      if (numRand > 60 &&  numRand < 96)
 	    	  gotasTipo.addGotaType(1);
-	      if (numRand >= 98)	
+	      if (numRand >= 96 && numRand<=98)	
 	    	  gotasTipo.addGotaType(3);
+	      if (numRand >= 99)	
+	    	  gotasTipo.addGotaType(4);
 	      
 	      lastDropTime = TimeUtils.nanoTime();
 	   }
 	
-   public void actualizarMovimiento(Tarro tarro) { 
+   public boolean actualizarMovimiento(TipoTarro tarro) { 
 	   // generar gotas de lluvia 
-	   
-	   if(TimeUtils.nanoTime() - lastDropTime + (tarro.getTime()*100)> 100000000) crearGotaDeLluvia();
+	   if(TimeUtils.nanoTime() - lastDropTime > 100000000) 
+		   crearObjetoObstaculo();
 	  
 	   
 	   // revisar si las gotas cayeron al suelo o chocaron con el tarro
@@ -68,37 +77,49 @@ public class Lluvia {
 		  Rectangle raindrop = gotasPos.getGota(i);
 	      raindrop.y -= 300 * Gdx.graphics.getDeltaTime();
 	      //cae al suelo y se elimina
-	      if(raindrop.y + 64 < 0) {
+	      if(raindrop.y + 32 < 0) {
 	    	  gotasPos.removerGota(i); 
 	    	  gotasTipo.removerGotaType(i);
 	      }
 	      if(raindrop.overlaps(tarro.getArea())) { //la gota choca con el tarro
 	    	if(gotasTipo.getGotaType(i)==1) { // gota dañina
-	    	  tarro.sumarPuntos(-20);
+	    	  tarro.sumarPuntos(-3);
 	    	  tarro.dañar();
+	    	  if (tarro.getVidas()<=0)
+	    		 return false; // si se queda sin vidas retorna falso /game over
 	    	  gotasPos.removerGota(i);  
 	    	  gotasTipo.removerGotaType(i);
-	    	  
-	      	}if(gotasTipo.getGotaType(i)==2) { // gota a recolectar
+	      	}
+	    	if(gotasTipo.getGotaType(i)==2){ // gota a recolectar
 	    	  tarro.sumarPuntos(1);
 	          dropSound.play();
 	          gotasPos.removerGota(i); 
 	          gotasTipo.removerGotaType(i);
 	      	}
-	      	
-	      	if(gotasTipo.getGotaType(i)==3) { // Super gota bendecida por jeováh
-		    	  tarro.sumarPuntos(20);
-		 
+	    	if(gotasTipo.getGotaType(i)==3){ // Super gota bendecida por jeováh
 		          tarro.curar();
 		          gotasPos.removerGota(i); 
 		          gotasTipo.removerGotaType(i);
 	      	}
+	      	
+	      	if(gotasTipo.getGotaType(i)==4) { // granizo
+	      		  tarro.sumarPuntos(-3);
+		    	  tarro.dañar(3);
+		    	  if (tarro.getVidas()<=0)
+		    		 return false; // si se queda sin vidas retorna falso /game over
+		    	  gotasPos.removerGota(i);  
+		    	  gotasTipo.removerGotaType(i);
+		          
+	      	}
+	      	
+	      	
 	      }
-	   }   
+	   } 
+	  return true; 
    }
    
-   public void actualizarDibujoLluvia(SpriteBatch batch) { 
-	
+   public void actualizarDibujoObjeto(SpriteBatch batch) { 
+	   
 	  for (int i=0; i < gotasPos.getSizeArray(); i++ ) {
 		  Rectangle raindrop = gotasPos.getGota(i);
 		  if(gotasTipo.getGotaType(i)==1) // gota mala
@@ -107,11 +128,20 @@ public class Lluvia {
 			 batch.draw(gotaBuena, raindrop.x, raindrop.y); 
 		  if(gotasTipo.getGotaType(i) == 3) // super gota 
 			 batch.draw(gotaMuyBuena, raindrop.x, raindrop.y); 
+		  if(gotasTipo.getGotaType(i) == 4) // super gota 
+			 batch.draw(granizo, raindrop.x, raindrop.y);
 	   }
    }
    public void destruir() {
-	      dropSound.dispose();
-	      rainMusic.dispose();
+      dropSound.dispose();
+      rainMusic.dispose();
    }
-   
+   public void pausar() {
+	  rainMusic.stop();
+   }
+   public void continuar() {
+	  rainMusic.play();
+   }
+	
+ 
 }
--- core/src/com/mygdx/game/Tarro.java
+++ core/src/com/mygdx/game/Tarro.java
@@ -4,94 +4,50 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 
 
-public class Tarro {
-	   private Rectangle bucket;
-	   private Texture bucketImage;
-	   private Sound sonidoHerido;
-	   private Sound sonidoHeal;
-	   private int vidas = 3;
-	   private int puntos = 0;
-	   private int velx = 400;
-	   private boolean herido = false;
-	   private int tiempoHeridoMax=50;
-	   private int tiempoHerido;
-	   private int time; int counter;
-	   //---
-
-	   //---
-	   
-	   public Tarro(Texture tex, Sound ss, Sound ss2) {
-		   bucketImage = tex;
-		   sonidoHerido = ss;
-		   sonidoHeal = ss2;
+public class Tarro extends TipoTarro {
+	  
+	  private int velx = 400;
+	  Sound healSound;
+	  
+	   public Tarro() {
+		   
+		   super(new Texture(Gdx.files.internal("bucket.png")), Gdx.audio.newSound(Gdx.files.internal("hurt.ogg")));
+		   this.healSound = Gdx.audio.newSound(Gdx.files.internal("goodSound.wav"));
+		   
 	   }
 	   
-		public int getVidas() {
-			return vidas;
-		}
-	
-		public int getPuntos() {
-			return puntos;
-		}
-		public Rectangle getArea() {
-			return bucket;
-		}
-		public void sumarPuntos(int pp) {
-			if(pp > 0)
-			puntos+=pp;
-			if(pp < 0)
-			puntos = puntos + pp;
+		public Rectangle getArea(){
+			return bucket.getBoundingRectangle();
 		}
 		
-		public int getTime() {
-			return time;
-		}
 
-		
-	
-	   public void crear() {
-
-		      bucket = new Rectangle();
-		      bucket.x = 800 / 2 - 64 / 2;
-		      bucket.y = 20;
-		      bucket.width = 40;
-		      bucket.height = 40;
-	   }
-	   public void dañar() {
-		  vidas--;
-		  herido = true;
-		  tiempoHerido=tiempoHeridoMax;
-		  sonidoHerido.play();
-	   }
 	   public void dibujar(SpriteBatch batch) {
 		 if (!herido)  
-		   batch.draw(bucketImage, bucket.x, bucket.y);
+		   bucket.draw(batch);
 		 else {
-	
-		   batch.draw(bucketImage, bucket.x, bucket.y+ MathUtils.random(-5,5));
+		
+		   bucket.setY(bucket.getY()+MathUtils.random(-5,5));
+		   bucket.draw(batch);
 		   tiempoHerido--;
 		   if (tiempoHerido<=0) herido = false;
 		 }
 	   } 
 	   
-	   
-	   public void curar() {
+	   @Override
+	   public void curar(){
 		   vidas++;
-		   sonidoHeal.play();
-	}
+		   sumarPuntos(20);
+		   healSound.play();
+	   }
 	   
-	public void temporizador(){ 
-		time++;
-		
-	}
-	
-
-	public void actualizarMovimiento() { 
+	   
+	   public void actualizarMovimiento() { 
 		   // movimiento desde mouse/touch
 		   /*if(Gdx.input.isTouched()) {
 			      Vector3 touchPos = new Vector3();
@@ -99,46 +55,22 @@ public class Tarro {
 			      camera.unproject(touchPos);
 			      bucket.x = touchPos.x - 64 / 2;
 			}*/
-		   //Movimiento desde teclado Formato flechas
-		   if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.UP)) bucket.y += velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) bucket.y -= velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.x -= velx * Gdx.graphics.getDeltaTime()*2;
-		   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.x += velx * Gdx.graphics.getDeltaTime()*2;
-		   if(Gdx.input.isKeyPressed(Input.Keys.UP) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.y += velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.y -= velx * Gdx.graphics.getDeltaTime();
+		   //movimiento desde teclado
+		   if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))  bucket.setX(bucket.getX()-velx * Gdx.graphics.getDeltaTime());
+		   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) bucket.setX(bucket.getX()+velx * Gdx.graphics.getDeltaTime());
+		   if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) bucket.setY(bucket.getY()+velx * Gdx.graphics.getDeltaTime());
+		   if(Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) bucket.setY(bucket.getY()-velx * Gdx.graphics.getDeltaTime());
+		   //Sprite
+		   if((Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) 
+				   && Gdx.input.isKeyPressed(Input.Keys.SPACE)) bucket.setX(bucket.getX()-velx * Gdx.graphics.getDeltaTime()*2);
+		   if((Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) 
+				   && Gdx.input.isKeyPressed(Input.Keys.SPACE)) bucket.setX(bucket.getX()+velx * Gdx.graphics.getDeltaTime()*2);
 		   
-		   // WASD format
-		   if(Gdx.input.isKeyPressed(Input.Keys.A)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.D)) bucket.x += velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.W)) bucket.y += velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.S)) bucket.y -= velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.A) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.x -= velx * Gdx.graphics.getDeltaTime()*2;
-		   if(Gdx.input.isKeyPressed(Input.Keys.D) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.x += velx * Gdx.graphics.getDeltaTime()*2;
-		   if(Gdx.input.isKeyPressed(Input.Keys.UP) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.y += velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && Gdx.input.isKeyPressed(Input.Keys.SPACE) ) bucket.y -= velx * Gdx.graphics.getDeltaTime();
-		   
-		   
-		   // Habilidad especial
-		   
-		   if(Gdx.input.isKeyPressed(Input.Keys.F)) 
-		  
 		   // que no se salga de los bordes izq y der
-		   if(bucket.x > 0) bucket.x = 0;
-		   if(bucket.x < 0) bucket.x = 0;
-		   if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-		   if(bucket.y < 0) bucket.y = 0;
-		   if(bucket.y > 480 - 64) bucket.y = 480 - 64;
+		   if(bucket.getX() < 0) bucket.setX(0);
+		   if(bucket.getX() > 1920 - 64) bucket.setX(1920 - 64);
+		   if(bucket.getY() < 0) bucket.setY(0);
+		   if(bucket.getY() > 1080 - 64) bucket.setY(1080 - 64);
 	   }
-	    
-
-	public void destruir() {
-		    bucketImage.dispose();
-	   }
-	
-   public boolean estaHerido() {
-	   return herido;
-   }
 	   
 }
